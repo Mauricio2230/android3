8767de7e5cb429a1ebf168508a742adcb1cfed65
Mauricio Sajama <mauricio.sajama22@gmail.com>
2021-07-12 09:06:48 -0400

    tercer commit

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -12,5 +12,8 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Toast.makeText(this, "hola", Toast.LENGTH_LONG).show();
+        Toast.makeText(this, "hola2", Toast.LENGTH_LONG).show();
+        Toast.makeText(this, "hola2", Toast.LENGTH_LONG).show();
+
     }
 }
